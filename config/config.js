cada3f1a22c72617ba9ec678a77b6502e2d9991d
robin <kashyap.robin.9277@gmail.com>
2022-03-23 16:25:55 +0530

    add config file

---
+++ config/config.js
@@ -0,0 +1,18 @@
+const config = {
+  jwtSecret: "thisisyummybay",
+  databaseUrl: "mongodb://localhost:27017/yummybay",
+  GOOGLE_CLIENT_ID:
+    "551621993702-c29obkogfq1cdrsjr98ma56qv1p3197f.apps.googleusercontent.com",
+  GOOGLE_CLIENT_SECRET: "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",
+  GOOGLE_REFRESH_TOKEN:
+    "1//04HGFUHm4hLPcCgYIARAAGAQSNwF-L9Ir3qrOTS1b9_e6dXQFJ8PAtmleRHhvSE-AnIqfefpm9H_PL86OJm0FxPE_a9xHLsou02o",
+  GOOGLE_REDIRECT_URI: "https://developers.google.com/oauthplayground",
+  DRIVE_FOLDER_ID: "1R0vC-FbgIBfInbPB91ZGQsZIS7dOfqKf",
+  TWILIO_SID: "ACbce9dd691a040d92343b2faebd905194",
+  TWILIO_TOKEN: "47921de7c05acf8128852c1c47c58731",
+  TWILIO_PHONE: "+18646616629",
+  FAST2SMS_KEY:
+    "PR4F0fHBwzxcSMv7gQGhl13y5ikCVYtTDO6rX2L8ab9WNAKndEywmBDRfWVoChuPE3TsFKYrQUdn8ivj",
+};
+
+export default config;
