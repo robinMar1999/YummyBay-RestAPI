920828174128f38dde4be270c41b2169260b7510
amanrana1512 <76261024+amanrana1512@users.noreply.github.com>
2022-03-23 16:25:48 +0530

    Update dish.js

--- src/models/dish.js
+++ src/models/dish.js
@@ -11,6 +11,18 @@ const dishSchema = new Schema({
     type: Number,
     required: true,
   },
+  category:{
+    type:String,
+    required :true,
+  },
+  type:{
+    type:String,
+    required:true,
+  },
+  description:{
+    type:String,
+    required:true,
+  },
   price: {
     type: Number,
     required: true,
